184408c4fa6a48f05da8df8133223ffc6b55b1c4
Samuel JS Wright <swrig109@uncc.edu>
2023-08-04 13:43:58 -0400

    changes made \

--- clrutils/impute.py
+++ clrutils/impute.py
@@ -3,16 +3,15 @@ import numpy as np
 from sklearn.impute import KNNImputer
 from sklearn.preprocessing import MinMaxScaler
 
-def impute_df(df,
-              subset=None,
-              scale=False,
-              neigh=5):
+
+def impute_df(df, subset=None, scale=False, neigh=5):
     """
     Applies imputation to a dataframe of numeric values.
 
     Parameters
     ----------
     df : pandas dataframe
+        Dataframe of chemical values to impute.
 
     subset : list-like, default None
         Subset of columns to apply numeric to.
@@ -28,47 +27,47 @@ def impute_df(df,
     temp_ND_NaN
         Pandas dataframe of imputed values.
     """
-        
+
     imputer_0 = KNNImputer(n_neighbors=neigh)
     imputer_1 = KNNImputer(n_neighbors=neigh)
     if subset is None:
-        subset=df.columns
+        subset = df.columns
 
     temp = np.abs(df[subset].copy().reset_index(drop=True))
 
     if scale:
         scaler = MinMaxScaler()
         temp = DataFrame(scaler.fit_transform(temp), columns=temp.columns)
-    
-    temp_knn = DataFrame(imputer_0.fit_transform(temp),
-                            columns=temp.columns)
-    
+
+    temp_knn = DataFrame(imputer_0.fit_transform(temp), columns=temp.columns)
+
     if not temp_knn.index.equals(temp.index):
         temp.index = temp_knn.index
-    
+
     temp.update(temp_knn)
 
     temp_ND_replace = df[subset].copy().reset_index(drop=True)
 
     temp_ND_NaN = temp_ND_replace.mask(temp_ND_replace > 0, np.NaN)
-    
+
     if not temp_ND_replace.index.equals(temp_ND_NaN.index):
-        temp_ND_NaN.index=temp_ND_replace.index
+        temp_ND_NaN.index = temp_ND_replace.index
 
     temp_ND_NaN.fillna(temp, inplace=True)
 
-    assert(temp_ND_NaN.isna().sum().sum()==0)
+    assert temp_ND_NaN.isna().sum().sum() == 0
 
     temp_ND_NaN.mask(temp_ND_NaN < 0, np.NaN, inplace=True)
 
     try:
-        assert(temp_ND_NaN.isna().sum().sum()>0)
+        assert temp_ND_NaN.isna().sum().sum() > 0
     except:
         print("No negative values in your dataset")
 
-    temp_ND_NaN_imputed = DataFrame(imputer_1.fit_transform(temp_ND_NaN),
-                                       columns=temp_ND_NaN.columns)
-    
+    temp_ND_NaN_imputed = DataFrame(
+        imputer_1.fit_transform(temp_ND_NaN), columns=temp_ND_NaN.columns
+    )
+
     if not temp_ND_NaN_imputed.index.equals(temp_ND_NaN.index):
         temp_ND_NaN_imputed.index = temp_ND_NaN.index
 
